a69f1e5519806bafdccedb1ad832223a36300f6f
Alvin Cabayan <alvincabayan@gmail.com>
2019-06-07 05:04:07 +1200

    [0001] Created initial API and access to db. Successfully tested the api connection with web client.

--- src/main/java/com/alvincabayan/api/MenuOrderApi.java
+++ src/main/java/com/alvincabayan/api/MenuOrderApi.java
@@ -8,6 +8,7 @@ package com.alvincabayan.api;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -19,20 +20,25 @@ import com.alvincabayan.model.MenuOrder;
 
 public interface MenuOrderApi {
 
+	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping(value = "/menuorder", produces = { "application/json" }, consumes = {
 			"application/json" }, method = RequestMethod.POST)
 	ResponseEntity<Void> addMenuOrder(@Valid @RequestBody MenuOrder body);
 
+	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping(value = "/menuorder/{idMenuOrder}", produces = { "application/json" }, method = RequestMethod.DELETE)
 	ResponseEntity<Void> deleteMenuOrder(Long idmenuorder, @RequestHeader(value = "api_key", required = false) String apiKey);
 
+	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping(value = "/menuorder/{idMenuOrder}", produces = { "application/json" }, method = RequestMethod.GET)
 	ResponseEntity<MenuOrder> getMenuOrderById(@PathVariable("idMenuOrder") Long idMenuOrder);
 
+	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping(value = "/menuorder", produces = { "application/json" }, consumes = {
 			"application/json" }, method = RequestMethod.PUT)
 	ResponseEntity<Void> updateMenuOrder(@Valid @RequestBody MenuOrder body);
 
+	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping(value = "/menuorder/{idMenuOrder}", produces = { "application/json" }, consumes = {
 			"application/x-www-form-urlencoded" }, method = RequestMethod.POST)
 	ResponseEntity<Void> updateMenuOrderWithForm(@PathVariable("idMenuOrder") Long idMenuOrder,
